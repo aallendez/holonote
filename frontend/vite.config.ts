c317bf7726e3ca694990425a85da077e1fcb5e1d
Juan Alonso-Allende <juan@aallende.com>
2025-11-27 22:35:33 +0100

    fix: release ci v1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    target: "es2022",
+  },
   ssr: {
     noExternal: ["firebase"],
   },
